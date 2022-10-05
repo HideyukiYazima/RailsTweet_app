09b7b4c34dd3e666868338f84b711f22023926ed
Yazima <yazima04200@gmail.com>
2022-10-06 03:33:37 +0900

    パスワードのハッシュ化を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,8 +58,8 @@ class UsersController < ApplicationController
   end
   
   def login
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/posts/index")
@@ -79,7 +79,6 @@ class UsersController < ApplicationController
   
   def likes
     @user = User.find_by(id: params[:id])
-    
     @likes = Like.where(user_id: @user.id)
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_05_173734) do
+ActiveRecord::Schema.define(version: 2022_10_05_181953) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2022_10_05_173734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
