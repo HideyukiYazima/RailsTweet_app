935f30906dcb119c9af08223d9a24f70b53955b7
Yazima <yazima04200@gmail.com>
2022-10-05 17:54:46 +0900

    ユーザー一覧追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_03_175405) do
+ActiveRecord::Schema.define(version: 2022_10_05_083632) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
